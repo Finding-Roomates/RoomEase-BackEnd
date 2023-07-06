84e86d7501237b3f953cf5cea0592a683af6e67b
Melvyn <melvyn9@gmail.com>
2023-07-05 19:58:43 -0700

    Setup MongoDB

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
-
+const mongoConnect = require('./util/database').mongoConnect;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello Shad");
 });
 
 app.get("/RoomEase", (req, res) => {
-  res.send("Testing");
+  res.send("Testing/Melvyn");
 });
 
 app.get("/Shad Testing Route", (req, res) => {
@@ -18,4 +18,6 @@ app.get("/Shad Testing Route Pt 2.2", (req, res) => {
   res.send("Shad Testing Pt 2.2");
 });
 
-app.listen(3000, () => console.log("Server is listening to port 3000"));
+mongoConnect( () => {
+  app.listen(3000, () => console.log("Server is listening to port 3000"));
+});
